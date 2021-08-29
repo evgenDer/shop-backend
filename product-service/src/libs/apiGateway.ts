44d158e7f11c5b6066fc4c9a3df39b4f51701058
Evgeniya Derevygo <evgeniya.nice@yandex.by>
2021-08-29 11:33:28 +0300

    fix: cors policy

--- product-service/src/libs/apiGateway.ts
+++ product-service/src/libs/apiGateway.ts
@@ -4,9 +4,13 @@ import type { FromSchema } from 'json-schema-to-ts';
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> };
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>;
 
-export const formatJSONResponse = (response: Record<string, unknown>, statusCode = 200) => {
+export const formatJSONResponse = (response: Record<string, unknown> | Array<Record<string, unknown>>, statusCode = 200) => {
   return {
     statusCode,
     body: JSON.stringify(response),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
   };
 };
