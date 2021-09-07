032c5be19874a7bbb1213b6be309a55d91ddec2b
Evgeniya Derevygo <evgeniya.nice@yandex.by>
2021-09-08 00:58:38 +0300

    feat: integrate existing lambdas with database

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,25 +1,54 @@
 import 'source-map-support/register';
 
+import { Client } from 'pg';
+
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import { getItemById } from '@libs/utils';
 
-import products from '../../../products.json';
+import { dbOptions } from '../../dbOptions';
 
 export const getProductById = async (event) => {
-  const { productId } = event.pathParameters;
+  let client;
 
-  const product = await getItemById(products, productId);
+  try {
+    const { productId } = event.pathParameters;
 
-  if (!product) {
+    console.log('Event: ', event, 'Product id: ', productId);
+
+    if (!productId) {
+      return formatJSONResponse({
+        message: 'Bad request',
+      }, 400);
+    }
+
+    client = new Client(dbOptions);
+    await client.connect();
+
+    const { rows: product } = await client.query(
+      'select p.*, s.count from products p left join stocks s on p.id = s.product_id where p.id=$1',
+      [productId]
+    );
+
+    if (!product) {
+      return formatJSONResponse({
+        message: 'Product not found',
+      }, 404);
+    }
+
     return formatJSONResponse({
-      message: 'Product not found',
-    }, 404);
-  }
+      product,
+    }, 200);
+  } catch(error) {
+    console.error(error);
 
-  return formatJSONResponse({
-    product,
-  }, 200);
+    return formatJSONResponse({
+      message: 'Internal server error',
+    }, 500);
+  } finally {
+    if (client) {
+      client.end();
+    }
+  }
 }
 
 export const main = middyfy(getProductById);
