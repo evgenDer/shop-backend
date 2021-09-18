4cf2567d42a6b4bc36ee369b43654fef5ed1060d
Evgeniya Derevygo <evgeniya.nice@yandex.by>
2021-09-19 01:07:52 +0300

    feat: add configuration for import-service

---
+++ import-service/serverless.ts
@@ -0,0 +1,42 @@
+import type { AWS } from '@serverless/typescript';
+
+import { BUCKET, REGION } from '@constants/aws';
+
+const serverlessConfiguration: AWS = {
+  service: 'import-service',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: REGION,
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 's3:ListBucket',
+        Resource: `arn:aws:s3:::${BUCKET}`,
+      },
+      {
+        Effect: 'Allow',
+        Action: 's3:*',
+        Resource: `arn:aws:s3:::${BUCKET}/*`,
+      },
+    ],
+  },
+};
+
+module.exports = serverlessConfiguration;
